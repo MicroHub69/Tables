20de20c3ad068a47543dee28249d9dbd64b06d02
Aselemi Divine <aselemidivine@gmail.com>
2022-11-15 15:43:07 -0800

    Tables Updated

--- src/Pages/Residence_tables/activityResidence/ActivityResidence.jsx
+++ src/Pages/Residence_tables/activityResidence/ActivityResidence.jsx
@@ -220,39 +220,6 @@ function ActivityResidence() {
             Activity residence
             <div className="event-container">
               <HandleSearchAndTab>
-                <div className="tabs">
-                  <span
-                    className={`tab ${checkActive(1, "active")}`}
-                    onClick={() => handleClick(1)}
-                  >
-                    <button onClick={(e) => setEvents(ActivityResidenceData)}>
-                      All
-                    </button>
-                  </span>
-
-                  <span
-                    className={`tab ${checkActive(2, "active")}`}
-                    onClick={() => handleClick(2)}
-                  >
-                    <button
-                      className="btn"
-                      onClick={() => filterEvents("Pending")}
-                    >
-                      Pending
-                    </button>
-                  </span>
-                  <span
-                    className={`tab ${checkActive(3, "active")}`}
-                    onClick={() => handleClick(3)}
-                  >
-                    <button
-                      className="btn"
-                      onClick={() => filterEvents("Resolved")}
-                    >
-                      Resolved
-                    </button>
-                  </span>
-                </div>
                 <div className="event-input">
                   <div class="search_set">
                     <img
